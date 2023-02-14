3c4c858186156ae1eba2153ddd291576b11eec89
komalASE <komal.kumari@habilelabs.io>
2023-02-14 16:23:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Page from './components/Page';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Here is our team!!</h1>
+      <Page />
+    </div>
+  );
+}
+
+export default App;
